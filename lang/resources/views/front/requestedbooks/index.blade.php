55a9137f854798b3e496b4f10cbc574debca464f
srdcpvtltd <debendraster@gmail.com>
2025-12-17 11:52:24 +0530

    removed

--- lang/resources/views/front/requestedbooks/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app') {{-- Adjust this if your layout is different --}}
-
-@section('content')
-    <h1>Requested Books</h1>
-    <p>This is the requested books page for users.</p>
-    {{-- You can add a table or list of requested books here --}}
-@endsection
